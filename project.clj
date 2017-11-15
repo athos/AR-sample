02dd733d8edfc91e0fde323dbcad7ec0236e4e1e
OHTA Shogo <athos0220@gmail.com>
2017-11-15 12:28:59 +0900

    first commit

---
+++ project.clj
@@ -0,0 +1,46 @@
+(defproject ar-sample "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :min-lein-version "2.7.1"
+
+  :dependencies [[org.clojure/clojure "1.9.0-RC1"]
+                 [org.clojure/clojurescript "1.9.946"]
+                 [org.clojure/core.async  "0.3.443"]]
+
+  :plugins [[lein-figwheel "0.5.14"]
+            [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
+
+  :source-paths ["src"]
+
+  :cljsbuild {:builds
+              [{:id "dev"
+                :source-paths ["src"]
+
+                :figwheel {:on-jsload "ar-sample.core/on-js-reload"
+                           :open-urls ["http://localhost:3449/index.html"]}
+
+                :compiler {:main ar-sample.core
+                           :asset-path "js/compiled/out"
+                           :output-to "resources/public/js/compiled/ar_sample.js"
+                           :output-dir "resources/public/js/compiled/out"
+                           :source-map-timestamp true
+                           :preloads [devtools.preload]}}
+               {:id "min"
+                :source-paths ["src"]
+                :compiler {:output-to "resources/public/js/compiled/ar_sample.js"
+                           :main ar-sample.core
+                           :optimizations :advanced
+                           :pretty-print false}}]}
+
+  :figwheel {:css-dirs ["resources/public/css"]}
+
+  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.4"]
+                                  [figwheel-sidecar "0.5.14"]
+                                  [com.cemerick/piggieback "0.2.2"]]
+                   :source-paths ["src" "dev"]
+                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+                   :clean-targets ^{:protect false} ["resources/public/js/compiled"
+                                                     :target-path]}})
