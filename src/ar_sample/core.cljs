36fb2e69947d07efe83d5410f5fdbe0784c4fb77
OHTA Shogo <athos0220@gmail.com>
2017-11-16 23:24:18 +0900

    Load fonts using FontLoader

--- src/ar_sample/core.cljs
+++ src/ar_sample/core.cljs
@@ -5,8 +5,12 @@
 
 (enable-console-print!)
 
+(def ^:private HELVETIKER_FONT_PATH
+  "/data/helvetiker_regular.typeface.json")
+
 (def +config+
   {:app-state {}
+   :fonts     {} ;; loaded dynamically
    :source    {}
    :renderer  {}
    :camera    {}
@@ -27,15 +31,28 @@
                :camera (ig/ref :camera)
                :context (ig/ref :context)}})
 
+(defmethod ig/init-key :fonts [_ fonts]
+  fonts)
+
 (defonce system (atom nil))
 
+(defn start [fonts]
+  (let [config (assoc +config+ :fonts fonts)]
+    (reset! system (ig/init config))))
+
+(defn stop [system]
+  (ig/halt! @system))
+
 (defn init []
-  (reset! system (ig/init +config+)))
+  (.load (js/THREE.FontLoader.) HELVETIKER_FONT_PATH
+    (fn [font]
+      (start {:helvetiker font}))))
 
 (events/listen js/window events/EventType.LOAD init)
 
 (defn on-js-reload []
   (print "reloading system ... ")
-  (ig/halt! @system)
-  (init)
+  (let [{:keys [fonts]} @system]
+    (stop system)
+    (start fonts))
   (println "reloading done."))
